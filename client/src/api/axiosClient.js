4cfc173e8ba96fafd6a2202f0ad8f91ed334bd8d
Your Name <amitakhileshtiwari@gmail.com>
2024-05-26 22:40:47 +0530

    initial

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseUrl = 'http://127.0.0.1:5000/api/v1/'
+const localBaseUrl = 'http://127.0.0.1:5000/api/v1/';
+const baseUrl = 'http://13.232.194.175:5000/api/v1/';
 const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
